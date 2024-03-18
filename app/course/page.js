95065284e8f4fb87edb1d093343b35cbf542a6a1
Mutiat Bashua <mutiatbashua@gmail.com>
2024-03-18 09:58:34 +0100

    ui design

--- app/course/page.js
+++ app/course/page.js
@@ -3,35 +3,33 @@
 import Link from "next/link";
 import Image from "next/image";
 import { coursesAvailable } from "../data";
-
-
+import CourseCard from "../components/CourseCards";
 
 const course = () => {
-  return (
-    <>
-      <div className="">
-        <p>
-          Welcome! We are here to make learning web3 lots more fun. Think about
-          us as the freecodecamp version of web3
-        </p>
-        <div>
-          Course Available
-          <div className="grid-cols-3 gap-12 grid w-5/6 m-auto">
-            {coursesAvailable.map((course) => (
-              <Link
-                href={`/course/${course.course_id}`}
-                className="rounded-lg shadow-2xl"
-                key={course.course_id}
-              >
-                <Image src={course.img} className="rounded-tl-lg rounded-tr-lg" />
-                <h2 className="text-xl">{course.title}</h2>
-              </Link>
-            ))}
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<section className="p-5 py-12 lg:px-20 lg:py-20 flex flex-col gap-5 lg:gap-7">
+			<div className="flex flex-col gap-3">
+				<h2 className="text-4xl font-semibold">Courses Available</h2>
+				<p className="w-[55%] font-light">
+					Welcome! We are here to make learning web3 lots more fun. Think
+					about us as the freecodecamp version of web3
+				</p>
+				<div>
+					<div className="w-fit mx-auto my-12 grid-cols-1 md:grid-cols-2 grid xl:grid-cols-3 gap-16  text-black">
+						{coursesAvailable.map((course, index) => (
+							<Link
+								href={`/course/${course.course_id}`}
+								className="rounded-lg shadow-xl"
+								key={course.course_id}
+							>
+								<CourseCard course={course} key={index} />
+							</Link>
+						))}
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default course;
